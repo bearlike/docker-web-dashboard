1ff09b881fea0f8b46b80723c072d29778735e0f
Krishnakanth Alagiri <admin@thekrishna.in>
2021-03-11 23:41:44 +0530

    Completed Single User Login (#2)
    * Static Login Page Complete
    - Credits: https://github.com/nauvalazhar/bootstrap-4-login-page
    
    * Pipelined Single User Login
    - Using environment variables

--- build-run.sh
+++ build-run.sh
@@ -2,5 +2,6 @@
 app="krishna/docker-cluster-dash"
 cont_name="cluster-dash-dev01"
 
+docker rm -f ${cont_name} || true
 docker build -t ${app} .
 docker run -d -p 56733:80 --name=${cont_name} ${app}
